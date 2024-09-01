c6175e76c71d9b0412d93d2222e1f65d3ed6e5bf
prakoi <gcg000sptt@gmail.com>
2024-09-01 11:45:51 +0700

    hot fixed

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use('/uploads/images', express.static(path.join(__dirname, './uploads/images')));
-app.use('/uploads/document', express.static(path.join(__dirname, './uploads/docs')));
+app.use('/uploads/docs', express.static(path.join(__dirname, './uploads/docs')));
 
 app.use(router);
 
